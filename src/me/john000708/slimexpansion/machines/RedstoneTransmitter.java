724afcef445a53fce68bf0602dcb919072ab5587
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-01 13:25:07 +0200

    Fixed indentation

--- src/me/john000708/slimexpansion/machines/RedstoneTransmitter.java
+++ src/me/john000708/slimexpansion/machines/RedstoneTransmitter.java
@@ -22,46 +22,46 @@ public class RedstoneTransmitter extends SimpleSlimefunItem<BlockTicker> {
 	public RedstoneTransmitter(Category category, ItemStack item, String id, RecipeType recipeType, ItemStack[] recipe) {
 		super(category, item, id, recipeType, recipe);
 	}
-	
+
 	@Override
 	public BlockTicker getItemHandler() {
 		return new BlockTicker() {
-			
+
 			@Override
-            public boolean isSynchronized() {
-                return false;
-            }
+			public boolean isSynchronized() {
+				return false;
+			}
 
-            @Override
-            public void tick(Block block, SlimefunItem slimefunItem, Config config) {
-                if (BlockStorage.getLocationInfo(block.getLocation(), "transmitterLoc") != null) {
-                    String[] serializedLoc = BlockStorage.getLocationInfo(block.getLocation(), "transmitterLoc").split(";");
-                    World world = Bukkit.getWorld(serializedLoc[0]);
-                    int x = Integer.valueOf(serializedLoc[1]);
-                    int y = Integer.valueOf(serializedLoc[2]);
-                    int z = Integer.valueOf(serializedLoc[3]);
+			@Override
+			public void tick(Block block, SlimefunItem slimefunItem, Config config) {
+				if (BlockStorage.getLocationInfo(block.getLocation(), "transmitterLoc") != null) {
+					String[] serializedLoc = BlockStorage.getLocationInfo(block.getLocation(), "transmitterLoc").split(";");
+					World world = Bukkit.getWorld(serializedLoc[0]);
+					int x = Integer.valueOf(serializedLoc[1]);
+					int y = Integer.valueOf(serializedLoc[2]);
+					int z = Integer.valueOf(serializedLoc[3]);
 
-                    Block transmitterBlock = world.getBlockAt(x, y, z);
+					Block transmitterBlock = world.getBlockAt(x, y, z);
 
-                    if (transmitterBlock.getType() == Material.AIR) {
-                        BlockStorage.addBlockInfo(block, "transmitterLoc", null);
-                        AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
-                        powerable.setPower(0);
-                        block.setBlockData(powerable);
-                        return;
-                    }
+					if (transmitterBlock.getType() == Material.AIR) {
+						BlockStorage.addBlockInfo(block, "transmitterLoc", null);
+						AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
+						powerable.setPower(0);
+						block.setBlockData(powerable);
+						return;
+					}
 
-                    int power = transmitterBlock.getBlockPower();
-                    new BukkitRunnable() {
-                        @Override
-                        public void run() {
-                            AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
-                            powerable.setPower(power);
-                            block.setBlockData(powerable); //TODO: make power dynamic
-                        }
-                    }.runTaskLater(SlimeXpansion.plugin, 1);
-                }
-            }
+					int power = transmitterBlock.getBlockPower();
+					new BukkitRunnable() {
+						@Override
+						public void run() {
+							AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
+							powerable.setPower(power);
+							block.setBlockData(powerable); //TODO: make power dynamic
+						}
+					}.runTaskLater(SlimeXpansion.plugin, 1);
+				}
+			}
 		};
 	}
 
